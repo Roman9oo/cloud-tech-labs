025aad58fa7665ad5e3c18089b6d1f7c9fb63551
Roman9oo <roman.bolonnyi.ri.2023@lpnu.ua>
2025-05-19 04:08:46 +0300

    feat: add save-course Lambda function with tf and JS

---
+++ save-course.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "lambda_save_course_role" {
+  name = "lambda-save-course-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_save_course_policy" {
+  name = "lambda-save-course-policy"
+  role = aws_iam_role.lambda_save_course_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["dynamodb:PutItem"],
+        Resource = "arn:aws:dynamodb:eu-central-1:761160581043:table/roman-dev-courses"
+      }
+    ]
+  })
+}
+
+data "archive_file" "lambda_save_course_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/save-course.js"
+  output_path = "${path.module}/lambda/save-course.zip"
+}
+
+resource "aws_lambda_function" "save_course" {
+  function_name = "save-course"
+  handler       = "save-course.handler"
+  runtime       = "nodejs18.x"
+  role          = aws_iam_role.lambda_save_course_role.arn
+  filename      = data.archive_file.lambda_save_course_zip.output_path
+  source_code_hash = data.archive_file.lambda_save_course_zip.output_base64sha256
+}
